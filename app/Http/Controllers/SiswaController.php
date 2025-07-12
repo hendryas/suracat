db23744bc5df11a6bb75159f1da74a53ff177802
Hendry Agus Setiawan <hendryas321@gmail.com>
2025-07-12 07:11:09 +0700

    Add management student

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class SiswaController extends Controller
+{
+    public function index()
+    {
+        $dataSiswa = User::where('role', 'siswa')->get();
+        return view('management.siswa.index', compact('dataSiswa'));
+    }
+
+    public function create()
+    {
+        // Jika kamu ingin pakai halaman terpisah (opsional)
+        return view('siswa.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama'     => 'required|string|max:255',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'  => 'error',
+                'message' => $validator->errors()->first()
+            ], 422);
+        }
+
+        $user = new User();
+        $user->name     = $request->nama;
+        $user->email    = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role     = 'siswa';
+        $user->save();
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Data siswa berhasil disimpan!'
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $siswa = User::findOrFail($id);
+        return response()->json($siswa); // digunakan untuk modal edit AJAX
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->name  = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data siswa berhasil diperbarui.'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+
+        if ($user->role !== 'siswa') {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User ini bukan siswa.'
+            ]);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data siswa berhasil dihapus.'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GuruController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,12 +20,21 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::prefix('admin')->group(function () {
+    // Route Manajemen Guru
     Route::get('/guru', [GuruController::class, 'index'])->name('guru.index');
     Route::get('/guru/create', [GuruController::class, 'create'])->name('guru.create');
     Route::post('/guru', [GuruController::class, 'store'])->name('guru.store');
     Route::get('/guru/{id}/edit', [GuruController::class, 'edit'])->name('guru.edit');
     Route::put('/guru/{id}', [GuruController::class, 'update'])->name('guru.update');
     Route::delete('/guru/{id}', [GuruController::class, 'destroy'])->name('guru.destroy');
+
+    // Route Manajemen Siswa
+    Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+    Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+    Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+    Route::get('/siswa/{id}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+    Route::put('/siswa/{id}', [SiswaController::class, 'update'])->name('siswa.update');
+    Route::delete('/siswa/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
 });
 
 require __DIR__ . '/auth.php';
